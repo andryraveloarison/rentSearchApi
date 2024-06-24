5227089549198cf77b97ee0e90abe7f142f4b045
raveloarison <andryraveloarison01@gmail.com>
2024-06-24 16:17:25 +0300

    securite

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -4,13 +4,30 @@ import src.controllers.extraction as extraction
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 import io
+from dotenv import load_dotenv
+import os
+import src.utils.loadData as loadData
 
 
 bp = Blueprint('api_bp', __name__)
 
 
+
+# Middleware pour vérifier la clé d'API
+def check_api_key(func):
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get('X-API-Key')
+        authorized_keys = loadData.authorized_keys
+        print(authorized_keys)
+        if api_key and api_key in authorized_keys:
+            return func(*args, **kwargs)
+        else:
+            return jsonify({'error': 'Clé d\'API invalide'}), 401
+    return wrapper
+
+
 @bp.route('/', methods=['POST'])
-@cross_origin()
+@check_api_key
 def classify():
     return extraction.extract(request.json['text'])
 
@@ -23,7 +40,6 @@ def transcript():
     
     audio_file = request.files['audio']
     file_stream = io.BytesIO(audio_file.read())
-    
     return transcription.transcribe(file_stream)
 
 
--- src/utils/loadData.py
+++ src/utils/loadData.py
@@ -1,9 +1,19 @@
 import whisper
 import json
+from dotenv import load_dotenv
+import os
 
 allLocationsPath = "data/locations.json"
 allMaterialsPath = "data/materials.json"
 allMaterialsTradPath = "data/materialsTrad.json"
+
+# Chargement des clés d'API à partir des variables d'environnement
+def load_api_keys():
+    api_keys = {}
+    for key in os.environ:
+        if key.startswith('API_KEY_'):
+            api_keys[os.getenv(key)] = f'key_description_{key[-1]}'  # Description basée sur le nom de la variable
+    return api_keys
 
 def loadLocations(allLocationsPath):
     with open(allLocationsPath, 'r', encoding='utf-8') as file:
@@ -24,3 +34,4 @@ model = whisper.load_model('small')
 allLocations = loadLocations(allLocationsPath)
 allMaterials = loadMaterials(allMaterialsPath)
 allMaterialsTrad = loadMaterialsTrad(allMaterialsTradPath)
+authorized_keys = load_api_keys()
